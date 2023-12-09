c580ba250584d35faa052a8a29113777e13ace49
jangyeseong757 <jangyeseong757@gmail.com>
2023-12-09 16:16:58 +0900

    입출력 기능 추가

--- Sources/accoutbook1.c
+++ Sources/accoutbook1.c
@@ -5,48 +5,77 @@
 #define MAX_EXPENSES 100
 #define MAX_DETAIL_LENGTH 50
 
+void saveExpenseToFile(const char* category, const char* detail) {
+    FILE* file;
+    if (fopen_s(&file, category, "a") != 0) {
+        printf("파일을 열 수 없습니다.\n");
+        return;
+    }
+
+    fprintf(file, "%s\n", detail); // Write detail to the file
+    fclose(file);
+}
+
+void showExpenseFromFile(const char* category) {
+    FILE* file;
+    if (fopen_s(&file, category, "r") != 0) {
+        printf("파일을 열 수 없습니다.\n");
+        return;
+    }
+
+    printf("\n%s의 지출 내역:\n", category);
+    char line[MAX_DETAIL_LENGTH];
+    int count = 0;
+    while (fgets(line, sizeof(line), file) != NULL) {
+        line[strcspn(line, "\n")] = 0; // Remove newline character if present
+        printf("%d. %s\n", ++count, line); // Display expense detail
+    }
+
+    fclose(file);
+}
+
 int main() {
     char categories[MAX_CATEGORIES][20] = { "식비", "학교생활", "의류", "기타" };
     int expenses_count[MAX_CATEGORIES] = { 0 };
-    char expenses[MAX_CATEGORIES][MAX_EXPENSES][MAX_DETAIL_LENGTH];
 
     printf("카테고리를 선택하세요:\n");
     for (int i = 0; i < MAX_CATEGORIES; i++) {
         printf("%d. %s\n", i + 1, categories[i]);
     }
 
-    int category_choice;
-    printf("선택(1-%d): ", MAX_CATEGORIES);
-    scanf_s("%d", &category_choice);
+    while (1) {
+        int category_choice;
+        printf("선택(1-%d) (0을 입력하면 종료): ", MAX_CATEGORIES);
+        scanf_s("%d", &category_choice);
 
-    if (category_choice < 1 || category_choice > MAX_CATEGORIES) {
-        printf("잘못된 선택입니다.\n");
-        return 1;
-    }
-
-    printf("%s를 선택하셨습니다.\n", categories[category_choice - 1]);
-
-    char detail[MAX_DETAIL_LENGTH];
-    printf("세부 내역및 지출의 이유를 입력해주세요 (예: 음식명:가격 지출 이유:): ");
-    scanf_s(" %[^\n]", detail);
-
-    int len = strlen(detail);
-    if (len < MAX_DETAIL_LENGTH) {
-        int i;
-        for (i = 0; i < len && i < MAX_DETAIL_LENGTH - 1; i++) {
-            expenses[category_choice - 1][expenses_count[category_choice - 1]][i] = detail[i];
+        if (category_choice == 0) {
+            printf("프로그램을 종료합니다.\n");
+            break;
         }
-        expenses[category_choice - 1][expenses_count[category_choice - 1]][i] = '\0';
+
+        if (category_choice < 1 || category_choice > MAX_CATEGORIES) {
+            printf("잘못된 선택입니다.\n");
+            continue;
+        }
+
+        printf("%s를 선택하셨습니다.\n", categories[category_choice - 1]);
+
+        char detail[MAX_DETAIL_LENGTH];
+        printf("세부 내역 및 지출의 이유를 입력해주세요 (예: 음식명:가격 지출 이유:): ");
+        getchar(); // Clear input buffer
+        fgets(detail, sizeof(detail), stdin);
+        detail[strcspn(detail, "\n")] = 0; // Remove newline character if present
+
+        if (strlen(detail) >= MAX_DETAIL_LENGTH) {
+            printf("입력이 너무 깁니다.\n");
+            continue;
+        }
+
+        saveExpenseToFile(categories[category_choice - 1], detail);
         expenses_count[category_choice - 1]++;
         printf("지출이 성공적으로 추가되었습니다!\n");
-    }
-    else {
-        printf("입력이 너무 깁니다.\n");
-    }
 
-    printf("\n%s의 지출 내역:\n", categories[category_choice - 1]);
-    for (int i = 0; i < expenses_count[category_choice - 1]; i++) {
-        printf("%d. %s\n", i + 1, expenses[category_choice - 1][i]);
+        showExpenseFromFile(categories[category_choice - 1]);
     }
 
     return 0;
