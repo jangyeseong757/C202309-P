56d6a8286a5e1a9931f0381675cd4a6f88945d9d
jangyeseong757 <jangyeseong757@gmail.com>
2023-12-09 20:51:44 +0900

    구조체 개념 추가

--- Sources/accoutbook1.c
+++ Sources/accoutbook1.c
@@ -1,55 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 
 #define MAX_CATEGORIES 4
-#define MAX_EXPENSES 100
 #define MAX_DETAIL_LENGTH 50
+#define MAX_EXPENSES 100
 
-void saveExpenseToFile(const char* category, const char* detail, int* cardCount, int* cashCount, int* otherCount) {
+
+struct Expense {
+    char category[20];
+    char detail[MAX_DETAIL_LENGTH];
+    int cardCount;
+    int cashCount;
+    int otherCount;
+};
+
+
+void saveExpenseToFile(struct Expense* expense);
+void showExpenseFromFile(struct Expense* expense);
+
+
+void saveExpenseToFile(struct Expense* expense) {
     FILE* file;
-    if (fopen_s(&file, category, "a") != 0) {
+    if (fopen_s(&file, expense->category, "a") != 0) {
         printf("파일을 열 수 없습니다.\n");
         return;
     }
 
-    fprintf(file, "%s\n", detail); // Write detail to the file
+    fprintf(file, "%s\n", expense->detail);
     fclose(file);
 
-    if (strstr(detail, "카드") != NULL) {
-        (*cardCount)++;
+    if (strstr(expense->detail, "(카드)") != NULL) {
+        expense->cardCount++;
     }
-    else if (strstr(detail, "현금") != NULL) {
-        (*cashCount)++;
+    else if (strstr(expense->detail, "(현금)") != NULL) {
+        expense->cashCount++;
     }
-    else if (strstr(detail, "기타") != NULL) {
-        (*otherCount)++;
+    else if (strstr(expense->detail, "(기타)") != NULL) {
+        expense->otherCount++;
     }
 }
 
-void showExpenseFromFile(const char* category, int cardCount, int cashCount, int otherCount) {
+
+void showExpenseFromFile(struct Expense* expense) {
     FILE* file;
-    if (fopen_s(&file, category, "r") != 0) {
+    if (fopen_s(&file, expense->category, "r") != 0) {
         printf("파일을 열 수 없습니다.\n");
         return;
     }
 
-    printf("\n%s의 지출 내역:\n", category);
+    printf("\n%s의 지출 내역:\n", expense->category);
     char line[MAX_DETAIL_LENGTH];
     int count = 0;
     while (fgets(line, sizeof(line), file) != NULL) {
-        line[strcspn(line, "\n")] = 0; // Remove newline character if present
-        printf("%d. %s\n", ++count, line); // Display expense detail
+        line[strcspn(line, "\n")] = 0;
+        printf("%d. %s\n", ++count, line);
     }
 
-    printf("\n카드: %d회, 현금: %d회, 기타: %d회\n", cardCount, cashCount, otherCount);
+    printf("\n카드: %d회, 현금: %d회, 기타: %d회\n", expense->cardCount, expense->cashCount, expense->otherCount);
 
     fclose(file);
 }
 
 int main() {
     char categories[MAX_CATEGORIES][20] = { "식비", "학교생활", "의류", "기타" };
-    int expenses_count[MAX_CATEGORIES] = { 0 };
+    struct Expense expenses[MAX_CATEGORIES];
     int cardCount = 0, cashCount = 0, otherCount = 0;
+
+    for (int i = 0; i < MAX_CATEGORIES; ++i) {
+        strcpy(expenses[i].category, categories[i]);
+        expenses[i].cardCount = 0;
+        expenses[i].cashCount = 0;
+        expenses[i].otherCount = 0;
+    }
 
     printf("카테고리를 선택하세요:\n");
     for (int i = 0; i < MAX_CATEGORIES; i++) {
@@ -59,7 +82,7 @@ int main() {
     while (1) {
         int category_choice;
         printf("선택(1-%d) (0을 입력하면 종료): ", MAX_CATEGORIES);
-        scanf_s("%d", &category_choice);
+        scanf("%d", &category_choice);
 
         if (category_choice == 0) {
             printf("프로그램을 종료합니다.\n");
@@ -74,10 +97,10 @@ int main() {
         printf("%s를 선택하셨습니다.\n", categories[category_choice - 1]);
 
         char detail[MAX_DETAIL_LENGTH];
-        printf("세부 내역 및 지출의 이유를 입력해주세요 (예: 음식명:가격 지출 방법): ");
-        getchar(); // Clear input buffer
+        printf("세부 내역 및 지출의 이유를 입력해주세요 (예: 음식명:가격): ");
+        getchar();
         fgets(detail, sizeof(detail), stdin);
-        detail[strcspn(detail, "\n")] = 0; // Remove newline character if present
+        detail[strcspn(detail, "\n")] = 0;
 
         if (strlen(detail) >= MAX_DETAIL_LENGTH) {
             printf("입력이 너무 깁니다.\n");
@@ -86,28 +109,38 @@ int main() {
 
         printf("지출 방법을 선택하세요 (1. 카드, 2. 현금, 3. 기타): ");
         int method_choice;
-        scanf_s("%d", &method_choice);
+        scanf("%d", &method_choice);
 
         switch (method_choice) {
         case 1:
-            strcat_s(detail, MAX_DETAIL_LENGTH, " (카드)");
+            strcat_s(detail, sizeof(detail), " (카드)");
+            cardCount++;
             break;
         case 2:
-            strcat_s(detail, MAX_DETAIL_LENGTH, " (현금)");
+            strcat_s(detail, sizeof(detail), " (현금)");
+            cashCount++;
             break;
         case 3:
-            strcat_s(detail, MAX_DETAIL_LENGTH, " (기타)");
+            strcat_s(detail, sizeof(detail), " (기타)");
+            otherCount++;
             break;
         default:
             printf("잘못된 선택입니다.\n");
             continue;
         }
 
-        saveExpenseToFile(categories[category_choice - 1], detail, &cardCount, &cashCount, &otherCount);
-        expenses_count[category_choice - 1]++;
-        printf("지출이 성공적으로 추가되었습니다!\n");
+        struct Expense currentExpense;
+        strcpy(currentExpense.category, categories[category_choice - 1]);
+        strcpy(currentExpense.detail, detail);
 
-        showExpenseFromFile(categories[category_choice - 1], cardCount, cashCount, otherCount);
+        saveExpenseToFile(&currentExpense);
+        int index = category_choice - 1;
+        expenses[index].cardCount = cardCount;
+        expenses[index].cashCount = cashCount;
+        expenses[index].otherCount = otherCount;
+
+        printf("지출이 성공적으로 추가되었습니다!\n");
+        showExpenseFromFile(&expenses[index]);
     }
 
     return 0;
